232f97d0746ff4b8315c310e9fdb9f3486ac0674
ananyachikkur-cs <ananyachikkur@gmail.com>
2025-11-15 06:15:39 +0530

    changes

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ function show_reportfounditem(){
     control_of_reportfounditem_section.style.display = "block";
 }
 
-let control_of_search_section = document.getElementById("searchitem");
-function show_seacrh_login(){
-    control_of_search_section.style.display = "block";
-}
-
 let control_of_contact_section = document.getElementById("contactus");
 function show_contact(){
     control_of_contact_section.style.display = "block";
@@ -24,18 +19,49 @@ function show_admin_login(){
     control_of_adminlogin.style.display ="block";
 }
 
-if (control_of_contact_section) {
-    control_of_contact_section.addEventListener('submit', function(e) {
-        e.preventDefault();
-        let name = document.getElementById("name").value;
-        let email = document.getElementById("email").value;
-        let message = document.getElementById("message").value;
 
-        const response = {
-            name, email, message
-        };
-     
-        let dummyDatabase = JSON
+let adminLoginForm = document.getElementById("adminLoginForm");
 
-    
+if (adminLoginForm) {
+    adminLoginForm.addEventListener('submit', function(e) {
+        e.preventDefault(); // Prevent default form submission
+        
+        let username = document.getElementById("adminUsername").value;
+        let password = document.getElementById("adminPassword").value;
+        
+      
+        const ADMIN_USERNAME = "admin";
+        const ADMIN_PASSWORD = "password123";
+        
+        if (username === ADMIN_USERNAME && password == ADMIN_PASSWORD) {
+            alert("Admin login successful!");
+            control_of_adminlogin.style.display ="none";
+            document.getElementById("userResponses").style.display ="block";
+        
+           
+        } else {
+            alert("Invalid admin username or password. Please try again.");
+            adminLoginForm.reset();
+        }
+    });
+
 }
+
+let control_of_contactme = document.getElementById("contactForm");
+control_of_contactme.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    const response = {
+    name ,email, message
+    }
+
+    let dummyDateBase = JSON.parse(localStorage.getItem('tempDp'))||[];
+    dummyDateBase.push(response);
+    localStorage.setItem('tempDB', JSON.stringify(DummyDatabase)); 
+
+
+});
+
